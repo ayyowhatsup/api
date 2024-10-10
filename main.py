85522cada3fec8029ef1f5fe1a9f9464d43cbb84
Dung Le Van <dung.levan@amela.vn>
2024-10-10 15:10:56 +0700

    refactor: logging and error handling in main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 Main module for the FastAPI application.
 """
 
-import logging
 import os
 from datetime import datetime
 from typing import Union
@@ -30,50 +29,6 @@ ydl_opts = {
     'skip_download': True,
     'quiet': True,
 }
-
-# # Configure logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# # Middleware to log request and response data
-
-
-@app.middleware("http")
-async def log_requests(request: Request, call_next):
-    """
-    Middleware function to log details of incoming HTTP requests and their responses.
-    Args:
-        request (Request): The incoming HTTP request.
-        call_next (Callable): The next middleware or route handler to be called.
-    Returns:
-        Response: The HTTP response from the next middleware or route handler.
-    Logs:
-        - Timestamp of the request.
-        - Client IP address.
-        - HTTP method of the request.
-        - URL of the request.
-        - Query parameters of the request.
-        - Status code of the response.
-    """
-    ip = request.client.host
-    method = request.method
-    url = str(request.url)
-    query_params = dict(request.query_params)
-    timestamp = datetime.utcnow().isoformat()
-
-    response = await call_next(request)
-
-    logger.info(
-        "%s - %s - %s %s - Query Params: %s - Response Status: %d",
-        timestamp,
-        ip,
-        method,
-        url,
-        query_params,
-        response.status_code
-    )
-
-    return response
 
 # Add CORS middleware
 app.add_middleware(
@@ -107,11 +62,8 @@ async def validate_api_key(request: Request, call_next):
     expected_api_keys = os.getenv("ALLOW_API_KEYS", "").split(",")
 
     if not api_key or api_key not in expected_api_keys:
-        return JSONResponse(
-            status_code=403,
-            content={"message": "Forbidden: Invalid or missing API key",
-                     "code": "invalid_api_key"}
-        )
+        raise ApiException({"message": "Forbidden: Invalid or missing API key",
+                           "code": "invalid_api_key", "status": 403})
 
     response = await call_next(request)
     return response
@@ -167,25 +119,23 @@ def get_yt_dlp_download_link(url: Union[HttpUrl, None] = None):
         raise ApiException(
             {"message": "Invalid URL", "code": "invalid_url", "status": 400}) from e
 
-    try:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(url, download=False)
-            video_url = info_dict.get('url', '')
-            if not video_url:
-                raise ApiException(data={
-                    "message": "No video URL found",
-                    "code": "no_video_url_found",
-                    "status": 400
-                })
-            return {
-                "data": {
-                    "url": video_url
-                },
-                "code": "success",
-                "message": "Request was successful"
-            }
-    except Exception as e:
-        raise ApiException({"status": 400, "message": str(e)}) from e
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(url, download=False)
+        video_url = info_dict.get('url', '')
+        if not video_url:
+            raise ApiException({
+                "message": "No video URL found",
+                "code": "no_video_url_found",
+                "status": 400
+            })
+
+        return {
+            "data": {
+                "url": video_url
+            },
+            "code": "success",
+            "message": "Request was successful"
+        }
 
 
 @ app.exception_handler(ApiException)
@@ -214,14 +164,9 @@ def handle_404_exception(_request, _exc):
     Args:
         _request: The request object that triggered the exception.
         exc: The exception instance.
-
-    Returns:
-        JSONResponse: A JSON response with a 404 status code and a message indicating that the resource was not found.
     """
-    return JSONResponse(
-        status_code=404,
-        content={"message": "Not found", "code": "not_found"}
-    )
+    raise ApiException({"message": "Resource not found",
+                       "code": "not_found", "status": 404})
 
 
 @app.exception_handler(RequestValidationError)
@@ -232,11 +177,6 @@ def handle_validation_error(_request, exc):
     Args:
         _request: The request object (not used in this function).
         exc: The exception that was raised during validation.
-
-    Returns:
-        JSONResponse: A response object containing the error message and a "bad_request" code.
     """
-    return JSONResponse(
-        status_code=400,
-        content={"message": str(exc), "code": "bad_request"}
-    )
+    raise ApiException({"message": str(exc),
+                       "code": "bad_request", "status": 400})
